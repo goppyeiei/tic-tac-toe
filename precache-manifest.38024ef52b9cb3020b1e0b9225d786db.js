770180617e7f29212d94190470bf0c4b689e772b
goppyeiei <goppyzixolo@gmail.com>
2020-09-09 10:04:57 +0700

    Updates

---
+++ precache-manifest.38024ef52b9cb3020b1e0b9225d786db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09064c40d13798c76beae15f8970174c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a37b5d0633a289f08393",
+    "url": "/static/css/main.b4da59bc.chunk.css"
+  },
+  {
+    "revision": "f8cd19e3cd2a96e0c8c3",
+    "url": "/static/js/2.546c7b0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.546c7b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37b5d0633a289f08393",
+    "url": "/static/js/main.004defa5.chunk.js"
+  },
+  {
+    "revision": "1bd38a07c745bab06b5a",
+    "url": "/static/js/runtime-main.377d2d02.js"
+  }
+]);
